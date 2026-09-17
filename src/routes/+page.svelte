df38e7ff4457fce2a30863d76232f62d2e94fa4a
Thomas Kleinendorst <thomas@kleinendorst.info>
2026-09-17 12:31:51 +0200

    Add margins to default page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,6 +2,10 @@
 	import Button from '$lib/components/ui/button/button.svelte';
 </script>
 
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
-<Button>A button</Button>
+<div class="m-2">
+	<h1>Welcome to SvelteKit</h1>
+	<p>
+		Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation
+	</p>
+</div>
+<Button class="m-2">A button</Button>
